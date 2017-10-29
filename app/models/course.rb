d46ddc03d255607c7e64daaff715f877ca25df2f
clairelu123 <32405800+clairelu123@users.noreply.github.com>
2017-10-29 00:25:13 -0700

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  validates :name, presence, length: {maximum: 50}
+  validates :name, presence： true, length: {maximum: 50}
   has_many :enrolls, :dependent => :destroy
   has_many :students, :through => :enrolls
   def display_name
